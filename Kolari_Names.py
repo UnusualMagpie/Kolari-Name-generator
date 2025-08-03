f93335e63480ff753675644a384978d5cb7b207b
UnusualMagpie <nunya@business.com>
2025-08-03 18:03:06 +0100

    More names

--- Kolari_Names.py
+++ Kolari_Names.py
@@ -1,15 +1,15 @@
 import random
 
 
-word1 = ["Eagle", "Swan", "Harrier", ""]
-word2 = ["talon", "beak","feather", "flight", "fall", "spots", "eye", "eagle", "song", "storm"]
-root1 = ["Elm", "Birch"]
+word1 = ["Mist", "Storm", "Strix", "Thrush", "Ural", "Swallow", "Swift", "Eagle", "Swan", "Harrier", "Heron", "Scops", "Kite", "Gale", "Tawny", "Hawk", "Gust", "Finch"]
+word2 = ["dusk","talon", "beak","feather", "flight", "fall", "spots", "eye", "eagle", "song", "storm", "dawn", "stream", "pool", "river", "wind", "wing", "sky"]
+forest = ["Willow", "Bramble", "Finch", "Elm", "Birch", "Oak", "Rowan", "Yew", "Pine", "Holly", "Dawn", "Dusk", "Robin", "Thrush"]
 choice = ""
 
 
-choice = input("For the Rootsingers?")
+choice = input("Any specific environment? ")
 
-if choice == "y":
-    print(random.choice(root1) + random.choice(word2))
+if choice == "Forest":
+    print(random.choice(forest) + random.choice(word2))
 else:
     print(random.choice(word1) + random.choice(word2))
