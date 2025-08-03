9740c4d825cd8e2e75c4f02e07ffdb93a083fd98
UnusualMagpie <nunya@business.com>
2025-08-03 17:34:35 +0100

    Extra words to be picked

--- Kolari_Names.py
+++ Kolari_Names.py
@@ -2,7 +2,14 @@ import random
 
 
 word1 = ["Eagle", "Swan", "Harrier", ""]
-word2 = ["Talon", "Beak"]
+word2 = ["talon", "beak","feather", "flight", "fall", "spots", "eye", "eagle", "song", "storm"]
+root1 = ["Elm", "Birch"]
+choice = ""
 
 
-print(random.choice(word1) + random.choice(word2))
+choice = input("For the Rootsingers?")
+
+if choice == "y":
+    print(random.choice(root1) + random.choice(word2))
+else:
+    print(random.choice(word1) + random.choice(word2))
