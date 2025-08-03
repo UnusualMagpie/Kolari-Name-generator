349dbc257981afd41d6c0d9f80520defe0eca2ab
UnusualMagpie <nunya@business.com>
2025-08-03 18:04:47 +0100

    Adding support for future coastal names

--- Kolari_Names.py
+++ Kolari_Names.py
@@ -4,6 +4,7 @@ import random
 word1 = ["Mist", "Storm", "Strix", "Thrush", "Ural", "Swallow", "Swift", "Eagle", "Swan", "Harrier", "Heron", "Scops", "Kite", "Gale", "Tawny", "Hawk", "Gust", "Finch"]
 word2 = ["dusk","talon", "beak","feather", "flight", "fall", "spots", "eye", "eagle", "song", "storm", "dawn", "stream", "pool", "river", "wind", "wing", "sky"]
 forest = ["Willow", "Bramble", "Finch", "Elm", "Birch", "Oak", "Rowan", "Yew", "Pine", "Holly", "Dawn", "Dusk", "Robin", "Thrush"]
+coast = ["Tern"]
 choice = ""
 
 
@@ -11,5 +12,7 @@ choice = input("Any specific environment? ")
 
 if choice == "Forest":
     print(random.choice(forest) + random.choice(word2))
+elif choice == "Coast":
+    print(random.choice(coast) + random.choice(word2))
 else:
     print(random.choice(word1) + random.choice(word2))
