8223863125d0bd5c8961c598ff9c69257e8a1184
UnusualMagpie <nunya@business.com>
2025-08-05 13:17:47 +0100

    Restructuring with subprograms to make adding additional features easier in future, adding support for single word names.

--- Kolari_Names.py
+++ Kolari_Names.py
@@ -1,18 +1,38 @@
 import random
 
-
+# Variables
 word1 = ["Mist", "Storm", "Strix", "Thrush", "Ural", "Swallow", "Swift", "Eagle", "Swan", "Harrier", "Heron", "Scops", "Kite", "Gale", "Tawny", "Hawk", "Gust", "Finch"]
 word2 = ["dusk","talon", "beak","feather", "flight", "fall", "spots", "eye", "eagle", "song", "storm", "dawn", "stream", "pool", "river", "wind", "wing", "sky"]
 forest = ["Willow", "Bramble", "Finch", "Elm", "Birch", "Oak", "Rowan", "Yew", "Pine", "Holly", "Dawn", "Dusk", "Robin", "Thrush"]
 coast = ["Tern"]
-choice = ""
+environment = ""
+name_length = ""
+
+# Subprograms to generate name depending on length
+
+def short_name(pName_environment):
+    if pName_environment.lower() == "forest":
+        return random.choice(forest)
+    elif pName_environment.lower() == "coast":
+        return random.choice(coast)
+    else:
+        return random.choice(word1)
+
+def long_name(pName_environment):
+    if pName_environment.lower() == "forest":
+        return random.choice(forest) + random.choice(word2)
+    elif pName_environment.lower() == "coast":
+        return random.choice(coast) + random.choice(word2)
+    else:
+        return random.choice(word1) + random.choice(word2)
 
 
-choice = input("Any specific environment? ")
 
-if choice == "Forest":
-    print(random.choice(forest) + random.choice(word2))
-elif choice == "Coast":
-    print(random.choice(coast) + random.choice(word2))
+
+name_length = int(input("How many words (1 or 2)? "))
+environment = input("Any specific environment? ")
+
+if name_length == 2:
+    print(long_name(environment))
 else:
-    print(random.choice(word1) + random.choice(word2))
+    print(short_name(environment))
