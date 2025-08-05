56a2b9b480d91fccf51be235c99882be377a5f65
UnusualMagpie <nunya@business.com>
2025-08-05 18:18:22 +0100

    Implementing a dictionary to reduce if statements, WIP

--- Kolari_Names.py
+++ Kolari_Names.py
@@ -1,30 +1,31 @@
 import random
 
+
+# Names dictionary
+names = {"forest":["Willow", "Bramble", "Finch", "Elm", "Birch", "Oak", "Rowan", "Yew", "Pine", "Holly", "Dawn", "Dusk", "Robin", "Thrush"],
+ "coast":["Tern"]}
+
+
+
+
+
+
 # Variables
 word1 = ["Mist", "Storm", "Strix", "Thrush", "Ural", "Swallow", "Swift", "Eagle", "Swan", "Harrier", "Heron", "Scops", "Kite", "Gale", "Tawny", "Hawk", "Gust", "Finch"]
 word2 = ["dusk","talon", "beak","feather", "flight", "fall", "spots", "eye", "eagle", "song", "storm", "dawn", "stream", "pool", "river", "wind", "wing", "sky"]
-forest = ["Willow", "Bramble", "Finch", "Elm", "Birch", "Oak", "Rowan", "Yew", "Pine", "Holly", "Dawn", "Dusk", "Robin", "Thrush"]
-coast = ["Tern"]
+#forest = ["Willow", "Bramble", "Finch", "Elm", "Birch", "Oak", "Rowan", "Yew", "Pine", "Holly", "Dawn", "Dusk", "Robin", "Thrush"]
+#coast = ["Tern"]
 environment = ""
 name_length = ""
 
 # Subprograms to generate name depending on length
 
 def short_name(pName_environment):
-    if pName_environment.lower() == "forest":
-        return random.choice(forest)
-    elif pName_environment.lower() == "coast":
-        return random.choice(coast)
-    else:
-        return random.choice(word1)
+    return random.choice(names[pName_environment.lower()])
+
 
 def long_name(pName_environment):
-    if pName_environment.lower() == "forest":
-        return random.choice(forest) + random.choice(word2)
-    elif pName_environment.lower() == "coast":
-        return random.choice(coast) + random.choice(word2)
-    else:
-        return random.choice(word1) + random.choice(word2)
+    return random.choice(names[pName_environment.lower()]) + random.choice(word2)
 
 
 
