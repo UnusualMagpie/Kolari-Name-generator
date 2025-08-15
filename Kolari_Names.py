28fb5dc3e205ba9535460323448a6d9e8b16e17e
UnusualMagpie <nunya@business.com>
2025-08-15 09:59:33 +0100

    Adding support for affinities, some more names added to generation options, adding a default option to the dictionary

--- Kolari_Names.py
+++ Kolari_Names.py
@@ -2,17 +2,21 @@ import random
 
 
 # Names dictionary
-names = {"forest":["Willow", "Bramble", "Elm", "Birch", "Oak", "Rowan", "Yew", "Pine", "Holly", "Thrush", "Briar", "Hazel"],
- "coast":["Tern", "Gull", "Tide"],
- "mountain": ["Stone", "Rock"]}
+names = {"forest":["Willow", "Bramble", "Elm", "Birch", "Oak", "Rowan", "Yew", "Pine", "Holly", "Thrush", "Briar", "Hazel", "Apple"],
+ "coast":["Tern", "Gull", "Tide", "Puffin", "Godwhit", "Knot", "Scoter", "Dunlin", "Whimbrel"],
+ "mountain": ["Stone", "Rock"],
+ "default": []}
 
 
 
 # Variables
-default = ["Mist", "Storm", "Strix", "Thrush", "Ural", "Swallow", "Swift", "Eagle", "Swan", "Harrier", "Heron", "Scops", "Kite", "Gale", "Tawny", "Hawk", "Gust", "Finch", "Dawn", "Dusk", "Cinder", "Crow", "Raven", "Quail", "Robin", "Linnet", "Lark"]
+default = ["Mist", "Storm", "Strix", "Thrush", "Ural", "Swallow", "Swift", "Eagle", "Swan", "Harrier", "Heron", "Scops", "Kite", "Gale", "Tawny", "Hawk", "Gust", "Finch", "Dawn", "Dusk", "Cinder", "Crow", "Raven", "Quail", "Robin", "Linnet", "Lark", "Shrike", "Sky", "Wind", "Breeze", "Feather", "Plume", "Cloud", "Wisp", "Gyr", "Peregrin", "Sparrow", "Raven", "Osprey", "Martin", ]
 word2 = ["dusk","talon", "beak","feather", "flight", "fall", "spots", "eye", "eagle", "song", "storm", "dawn", "stream", "pool", "river", "wind", "wing", "sky"]
+
 environment = ""
 name_length = ""
+
+affinities = ["Rain", "Mist", "Smoke", "Sand", "Mud", "Fireflies", "Wind", "Freshwater", "Clouds", "Rock", "Plants", "Growth", "Ice", "Wood", "Growth", "Butterfly", "Metal", "Sunlight", "Amber", "Heat", "Embers", "Fire", "Dust", "Crystal", "Lightning", "Snow", "Frost", "Aurora", "Ripples", "Gold", "Ash", "Underwater light", "Stars", "Tar", "Waterfall", "Sea foam", "Night sky", "Fungi", "Pollen", "Sunset clouds", "Scree", "Moonlight", "Thunder", "Dawn", "Bone", "Constellations", "Tides", "Waves", "Clay"]
 
 # Subprograms to generate name depending on length
 
@@ -33,5 +37,7 @@ environment = input("Any specific environment? ")
 
 if name_length == 2:
     print(long_name(environment))
+    print(random.choice(affinities))
 else:
     print(short_name(environment))
+    print(random.choice(affinities))
